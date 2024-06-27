38ad505997be412dbf628d3aec7493029ba0a8f4
hanba-gu1 <reinlein.k.smile14@gmail.com>
2024-06-27 10:23:21 +0900

    indent

--- desktop.ps1
+++ desktop.ps1
@@ -5,16 +5,15 @@ $MyWallpaper=$jsonData.desktop.image_path
 $code = @' 
 using System.Runtime.InteropServices; 
 namespace Win32{ 
-	 
-	  public class Wallpaper{ 
-		  [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-			static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
-			
-			public static void SetWallpaper(string thePath){ 
-				SystemParametersInfo(20,0,thePath,3); 
-			}
-	 }
- } 
+  public class Wallpaper{ 
+    [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+    static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+    
+    public static void SetWallpaper(string thePath){ 
+      SystemParametersInfo(20,0,thePath,3); 
+    }
+  }
+} 
 '@
 
 add-type $code 
