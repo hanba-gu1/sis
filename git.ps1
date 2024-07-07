aa35e18f4b8d42b65b7c427103050d83390edfdc
hanba-gu1 <reinlein.k.smile14@gmail.com>
2024-07-08 00:22:34 +0900

    git2

--- git.ps1
+++ git.ps1
@@ -8,13 +8,3 @@ if (-not (Test-Path $tempdir)) {
 }
 
 Start-Process powershell -Args "$env:SIS_ROOT\git-install.ps1 $tempdir" -Verb RunAs
-
-# Start-Process powershell.exe "
-# 	Set-Location $($git.installer_move_path)\WindowsInfrastructureExample\
-# 	.\install-git.ps1
-# 	.\set-git-path.ps1
-# 	C:\PROGRA~1\Git\cmd\git.exe config --global user.name $($git.username)
-# 	C:\PROGRA~1\Git\cmd\git.exe config --global user.email $($git.email)
-# 	C:\PROGRA~1\Git\cmd\git.exe config --global http.proxy $env:http_proxy
-# 	C:\PROGRA~1\Git\cmd\git.exe 
-# " -Verb RunAs
