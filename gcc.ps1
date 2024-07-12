eb073515f7d2c062148cc38618b69c74f97ff957
hanba-gu1 <reinlein.k.smile14@gmail.com>
2024-07-12 16:18:12 +0900

    gcc latest

--- gcc.ps1
+++ gcc.ps1
@@ -2,7 +2,7 @@ Param([bool]$flag)
 if(-not $flag){ exit 0 }
 
 $tempdir = "$env:TEMP_DIR\gcc"
-$installer_link = "https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev0/x86_64-13.2.0-release-win32-seh-msvcrt-rt_v11-rev0.7z"
+$installer_link = "https://github.com/niXman/mingw-builds-binaries/releases/latest/download/x86_64-13.2.0-release-win32-seh-msvcrt-rt_v11-rev0.7z"
 $installer_path = "$tempdir\mingw64.7z"
 $program_dir = "$tempdir\mingw64"
 $bin_dir = "$program_dir\bin"
