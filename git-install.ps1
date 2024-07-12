49102db23420843cb1e1e11f86b0fd219e6faa30
hanba-gu1 <reinlein.k.smile14@gmail.com>
2024-07-12 15:38:22 +0900

    sis_auto_update

--- git-install.ps1
+++ git-install.ps1
@@ -15,25 +15,3 @@ Set-Location $installer_dir
 Invoke-WebRequest $exe_link -OutFile Git-2.36.1-64-bit.exe
 .\install-git.ps1
 .\set-git-path.ps1
-
-Add-Type -AssemblyName System.Web
-
-C:\PROGRA~1\Git\cmd\git.exe config --global user.name $username
-C:\PROGRA~1\Git\cmd\git.exe config --global user.email $email
-
-Add-Type -AssemblyName System.Web
-
-$proxyUser = Get-Content "~\sis-proxy\username.txt"
-$encryptedPassword = Get-Content "~\sis-proxy\password.txt"
-$key = Get-Content "~\sis-proxy\key.bin" -Encoding Byte
-
-$securePassword = $encryptedPassword | ConvertTo-SecureString -Key $key
-$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
-$proxyPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstr)
-[System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr)
-
-$proxy = "http://$($proxyUser):$([System.Web.HttpUtility]::UrlEncode($proxyPassword))@10.1.100.111:8080"
-
-C:\PROGRA~1\Git\cmd\git.exe config --global http.proxy $proxy
-
-C:\PROGRA~1\Git\cmd\git.exe config --global safe.directory *
--- git.ps1
+++ git.ps1
@@ -1,4 +1,4 @@
-Param([bool]$flag, [string]$username, [string]$email)
+Param([bool]$flag, [string]$username, [string]$email, [bool]$sis_auto_update)
 if(-not $flag){ exit 0 }
 
 $tempdir = "$env:TEMP_DIR\git"
@@ -10,4 +10,31 @@ if (-not (Test-Path $tempdir)) {
 $script_move_path = "$tempdir\git-install.ps1"
 Copy-Item .\git-install.ps1 $script_move_path
 
-Start-Process powershell -Args "$script_move_path $tempdir $username $email" -Verb RunAs
+Start-Process powershell -Args "$script_move_path $tempdir $username $email" -Verb RunAs -Wait
+
+
+Add-Type -AssemblyName System.Web
+
+C:\PROGRA~1\Git\cmd\git.exe config --global user.name $username
+C:\PROGRA~1\Git\cmd\git.exe config --global user.email $email
+
+Add-Type -AssemblyName System.Web
+
+$proxyUser = Get-Content "~\sis-proxy\username.txt"
+$encryptedPassword = Get-Content "~\sis-proxy\password.txt"
+$key = Get-Content "~\sis-proxy\key.bin" -Encoding Byte
+
+$securePassword = $encryptedPassword | ConvertTo-SecureString -Key $key
+$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+$proxyPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstr)
+[System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr)
+
+$proxy = "http://$($proxyUser):$([System.Web.HttpUtility]::UrlEncode($proxyPassword))@10.1.100.111:8080"
+
+C:\PROGRA~1\Git\cmd\git.exe config --global http.proxy $proxy
+
+C:\PROGRA~1\Git\cmd\git.exe config --global safe.directory *
+
+if ($sis_auto_update) {
+    C:\PROGRA~1\Git\cmd\git.exe pull
+}
